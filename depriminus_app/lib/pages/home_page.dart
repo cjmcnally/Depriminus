2e757042dc09f1cf52d5c0a4313b1edd2c8e3cb7
Colin <colinmcnallyj@gmail.com>
2022-09-28 00:41:23 -0400

    Created Exercise List Section
    Will need to add different exercises in the section, icon buttons most likely.

--- depriminus_app/lib/pages/home_page.dart
+++ depriminus_app/lib/pages/home_page.dart
@@ -19,131 +19,178 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.deepPurple[800],
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              child: Column(
                 children: [
-                  // Welcome message
-                  Text(
-                    'Hello Colin!',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Welcome message
+                      Text(
+                        'Hello Colin!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // Settings Icon
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple[600],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.settings,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                  // Settings Icon
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[600],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Icon(
-                      Icons.settings,
-                      color: Colors.white,
-                    ),
+
+                  SizedBox(
+                    height: 40,
+                  ),
+
+                  // How do you feel?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'How do you feel?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(
+                    height: 25,
+                  ),
+                  // Mood tracker
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // bad
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: ''),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            'Bad',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // fine
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: ''),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            'Fine',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // good
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: ''),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            'Good',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // amazing
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: ''),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            'Amazing',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
-
-              SizedBox(
-                height: 40,
-              ),
-
-              // How do you feel?
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'How do you feel?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // Section for daily advice
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
                   ),
-                ],
-              ),
-
-              SizedBox(
-                height: 25,
-              ),
-
-              // Mood tracker
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // bad
-                  Column(
+                ),
+                child: Center(
+                  child: Column(
                     children: [
-                      EmoticonFace(emoticonFace: ''),
-                      SizedBox(
-                        height: 7,
+                      // exercise heading with sort icon
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Exercises',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                            ),
+                          ),
+                          Icon(Icons.sort_sharp),
+                        ],
                       ),
-                      Text(
-                        'Bad',
-                        style: TextStyle(
-                          color: Colors.white,
+
+                      // list view of exercises - scrollable
+                      Expanded(
+                        child: ListView(
+                          scrollDirection: Axis.vertical,
                         ),
                       ),
                     ],
                   ),
-
-                  // fine
-                  Column(
-                    children: [
-                      EmoticonFace(emoticonFace: ''),
-                      SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Fine',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // good
-                  Column(
-                    children: [
-                      EmoticonFace(emoticonFace: ''),
-                      SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Good',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // amazing
-                  Column(
-                    children: [
-                      EmoticonFace(emoticonFace: ''),
-                      SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        'Amazing',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
